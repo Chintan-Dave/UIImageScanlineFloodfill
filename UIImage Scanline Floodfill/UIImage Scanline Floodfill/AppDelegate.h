2273fa9503ebeeec841f90335ae19aac4a3df189
Chintan R Dave <chintan@zealousweb.com>
2013-07-15 20:17:41 +0530

    Create Project
    Create New iOS project.

---
+++ UIImage Scanline Floodfill/UIImage Scanline Floodfill/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIImage Scanline Floodfill
+//
+//  Created by chintan on 15/07/13.
+//  Copyright (c) 2013 ZWT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIImage Scanline Floodfill/UIImage Scanline Floodfill/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UIImage Scanline Floodfill
+//
+//  Created by chintan on 15/07/13.
+//  Copyright (c) 2013 ZWT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
