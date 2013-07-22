311cc33f2b758b60c949e660bca41e0c370d4d8d
Chintan R Dave <chintan@zealousweb.com>
2013-07-22 15:11:30 +0530

    Add Condition on y to solve crash issue.

---
+++ Source Code/UIImage+FloodFill.h
@@ -0,0 +1,16 @@
+//
+//  UIImage+FloodFill.h
+//  ImageFloodFilleDemo
+//
+//  Created by chintan on 15/07/13.
+//  Copyright (c) 2013 ZWT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LinkedListStack.h"
+
+@interface UIImage (FloodFill)
+
+- (UIImage *) floodFillFromPoint:(CGPoint)startPoint withColor:(UIColor *)newColor andTolerance:(int)tolerance;
+
+@end
--- UIImage Scanline Floodfill/UIImage Scanline Floodfill/ViewController.h
+++ UIImage Scanline Floodfill/UIImage Scanline Floodfill/ViewController.h
@@ -9,7 +9,6 @@
 #import <UIKit/UIKit.h>
 #import "UIImage+FloodFill.h"
 #import "FloodFillImageView.h"
-#import <QuartzCore/QuartzCore.h>
 
 @interface ViewController : UIViewController
 
